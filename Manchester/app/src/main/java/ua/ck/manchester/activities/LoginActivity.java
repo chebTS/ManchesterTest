9bcd3cf69b03a5ecb637a55b001fd79ae6c5d225
chebts <chebTS@gmail.com>
2016-03-09 11:34:37 +0000

    init

---
+++ Manchester/app/src/main/java/ua/ck/manchester/activities/LoginActivity.java
@@ -0,0 +1,9 @@
+package ua.ck.manchester.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by cheb on 3/9/16.
+ */
+public class LoginActivity extends AppCompatActivity {
+}
