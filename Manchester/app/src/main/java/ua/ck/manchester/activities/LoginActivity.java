ed17364d2144aa58b2984f62be584275c160a25f
chebts <chebTS@gmail.com>
2016-03-09 11:39:56 +0000

    Login layout

--- Manchester/app/src/main/java/ua/ck/manchester/activities/LoginActivity.java
+++ Manchester/app/src/main/java/ua/ck/manchester/activities/LoginActivity.java
@@ -1,9 +1,31 @@
 package ua.ck.manchester.activities;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import ua.ck.manchester.R;
+
 
 /**
  * Created by cheb on 3/9/16.
  */
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        findViewById(R.id.btn_login).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_login:
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                break;
+        }
+    }
 }
